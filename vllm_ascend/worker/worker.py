a7d59da388ec9c1d252d69884b2caacf8de06464
hust17yixuan <303660421@qq.com>
2026-04-16 10:47:33 +0800

    adapt hixel
    Signed-off-by: hust17yixuan <303660421@qq.com>

--- vllm_ascend/worker/worker.py
+++ vllm_ascend/worker/worker.py
@@ -49,7 +49,7 @@ from vllm.v1.worker.workspace import init_workspace_manager
 import vllm_ascend.envs as envs_ascend
 from vllm_ascend.ascend_config import get_ascend_config, init_ascend_config
 from vllm_ascend.batch_invariant import init_batch_invariance
-from vllm_ascend.cpu_binding import bind_cpus
+from vllm_ascend.cpu_binding import bind_cpus, DeviceInfo
 from vllm_ascend.device_allocator.camem import CaMemAllocator
 from vllm_ascend.distributed.parallel_state import init_ascend_model_parallel
 from vllm_ascend.ops.triton.triton_utils import init_device_properties_triton
@@ -255,12 +255,29 @@ class NPUWorker(WorkerBase):
         # Note that this should be imported after torch.npu.set_device
         # to avoid repeated set_device in extra processes
         from vllm.triton_utils import HAS_TRITON
+        import os, json
 
         if HAS_TRITON:
             import torch_npu._inductor  # noqa: F401
 
         gc.collect()
         torch.npu.empty_cache()
+        
+        if get_ascend_device_type() == AscendDeviceType.A5:
+            visible_devices = os.getenv("ASCEND_RT_VISIBLE_DEVICES")
+            if visible_devices is None: 
+                devices = sorted(list(DeviceInfo.get_npu_map_info().keys()))
+            else:
+                devices = [int(x) for x in visible_devices.split(",")]
+            local_comm_res_path = os.environ.get("ASCEND_LOCAL_COMM_RES_PATH")
+
+            if local_comm_res_path:
+                local_comm_res_file = os.path.join(local_comm_res_path, f"ub_endpoint_npu_{devices[self.local_rank]}.json")
+                with open(local_comm_res_file, 'r') as f:
+                    data = json.load(f)
+                    
+                local_comm_res_str = json.dumps(data, ensure_ascii=False, separators=(',', ':'))
+                os.environ["ASCEND_LOCAL_COMM_RES"] = local_comm_res_str
 
         # take current memory snapshot
         self.init_snapshot = MemorySnapshot()
